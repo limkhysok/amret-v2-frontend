aca277ce98df15409f9506adb77853859d3ba29f
Sok Limkhy <soklimkhy@gmail.com>
2026-03-06 06:17:21 -0500

    feat: Initialize new Angular frontend application with core setup, layout components, and initial feature pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Clients } from './pages/clients/clients';
+import { Loans } from './pages/loans/loans';
+import { Reports } from './pages/reports/reports';
+import { Settings } from './pages/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'clients', component: Clients },
+    { path: 'loans', component: Loans },
+    { path: 'reports', component: Reports },
+    { path: 'settings', component: Settings },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' }
+];
